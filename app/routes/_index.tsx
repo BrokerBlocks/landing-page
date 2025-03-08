9f1bf12a9198959f08175f6f26f6e11eb058db06
Alfredo Medina <alfredo.medina@stxnext.pl>
2025-03-07 18:56:40 -0600

    Implement basic landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,40 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node"
+import type { LinksFunction } from "@remix-run/node"
 
 export const meta: MetaFunction = () => {
   return [
     { title: "BrokerBlocks" },
-    { name: "description", content: "An AI-powered sales intelligence and process automation platform designed to help real estate professionals improve prospect engagement and conversion" },
+    { name: "description", content: "BrokerBlocks is the AI-powered sales intelligence platform built for real estate professionals who want to engage smarter, close faster, and scale effortlessly." },
   ];
 };
+
+import styles from "~/styles/index.css?url";
+import bbLogo from "../../public/bb_logo.png"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export default function Index() {
   return (
     <div>
       <h1>BrokerBlocks</h1>
+      <center>
+        <img src={bbLogo} alt="BrokerBlocks logo" />
+      </center>
+      <center>
+        <i>Stealth Mode - Invitation Only</i>
+      </center>
+      <center className="break big">
+        Turn More Leads into Clients. <br />
+        Turn More Clients into Raving Fans!
+      </center>
+      <center className="break">
+        <a href="/" className="linkBtn">Apply for Early Access</a>
+      </center>
+      <center className="break">
+        <small>Questions? Reach out at support@brokerblocks.com</small>
+      </center>
     </div>
   );
 }
